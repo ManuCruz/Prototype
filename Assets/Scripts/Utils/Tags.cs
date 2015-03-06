26608f65db54757ca02a8bdfb12d45cba8849c86
Alfredo Sosa <alfres92@gmail.com>
2015-03-06 18:05:26 +0100

    Empezando rotaciones.

---
+++ Assets/Scripts/Utils/Tags.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags {
+	public const string player = "Player";
+	public const string camera = "MainCamera";
+	public const string world = "World";
+	
+
+}
