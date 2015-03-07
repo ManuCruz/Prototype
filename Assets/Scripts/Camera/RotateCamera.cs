81c5f5a8559bacd5513ac14c751f99b7734a3c3d
Alfredo Sosa <alfres92@gmail.com>
2015-03-07 15:51:05 +0100

    Escena Asosa: 	- Añadidos triggers de rotacion de camara(movimiento inmediato) hay que hacer que interpolen. 	- Eliminado script followCharacter Original.

---
+++ Assets/Scripts/Camera/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour {
+
+	public Vector3 position;
+	public Camera mainCamera;
+
+	private GameObject world;
+	void Start(){
+		world = GameObject.FindGameObjectWithTag (Tags.world);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == Tags.player) {
+			mainCamera.transform.position = position;
+			mainCamera.transform.LookAt(world.transform.position);
+		}
+	}
+}
