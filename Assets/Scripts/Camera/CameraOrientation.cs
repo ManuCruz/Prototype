7d65b4a7fc80e18f2a202d099887e7bb4dafa0c4
Alfredo Sosa <alfres92@gmail.com>
2015-03-08 19:48:17 +0100

    - Escnea Asosa: 	- Transiciones de camra por cara.(Modificado todos los scripts) 	- Queda mal las transciones sobre el eje Y. La camara da golpes bruscos de rotacion para mirar siempre al 0,0,0. - Escena Asosa1: 	- Prueba sobre la escena de manolo con movimiento del personaje.

---
+++ Assets/Scripts/Camera/CameraOrientation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrientation : MonoBehaviour {
+
+	public Vector3 endPosition;
+	public Camera mainCamera;
+	public float speed = 3.0f;
+	
+	private GameObject m_world;
+	private FollowCharacter m_follow;
+	private bool m_centerCamera;
+
+	void Start () {
+		m_follow = mainCamera.GetComponent<FollowCharacter>();
+		m_world = GameObject.FindGameObjectWithTag(Tags.world);
+		m_centerCamera = false;
+	}
+
+	void Update () {
+		if (m_centerCamera) {
+			if (mainCamera.transform.position == endPosition) {
+				mainCamera.transform.position = endPosition;
+			} else {
+				mainCamera.transform.position = Vector3.Slerp (mainCamera.transform.position, endPosition, speed * Time.deltaTime);
+				mainCamera.transform.LookAt (m_world.transform.position);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == Tags.player) {
+			m_follow.enable = false;
+			m_centerCamera = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other){
+		if (other.tag == Tags.player) {
+			m_follow.enable = true;
+			m_centerCamera = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Camera/FollowCharacter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCharacter : MonoBehaviour {
+
+	public bool enable = true;
+	public float distance = 15f;
+	public float speed = 1.0f;
+	
+	private GameObject m_player;
+	private GameObject m_world;
+	void Start () {
+		m_player = GameObject.FindGameObjectWithTag(Tags.player);
+		m_world = GameObject.FindGameObjectWithTag(Tags.world);
+	}
+	
+	void Update () {
+		if (enable) {
+			Vector3 pos = m_player.transform.position.normalized;
+			Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, pos * distance, speed * Time.deltaTime);
+			Camera.main.transform.LookAt(m_world.transform.position);
+		}
+	}
+}
