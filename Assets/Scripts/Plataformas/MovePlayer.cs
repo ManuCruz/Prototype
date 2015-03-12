b649e1cae1c5aaed475337c337d5fe0ff7696a46
Alfredo Sosa <alfres92@gmail.com>
2015-03-12 14:57:17 +0100

    Añadidas plataformas moviles.

---
+++ Assets/Scripts/Plataformas/MovePlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	private float m_velocity;
+	private MovilePlatform m_movile;
+	void Start(){
+		m_movile = transform.parent.gameObject.GetComponent<MovilePlatform>();
+		m_velocity = m_movile.speed;
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == Tags.playerFeet) {
+			other.transform.parent.transform.Translate(transform.right * m_velocity * Time.deltaTime * m_movile.getActualDirection(), Space.World);
+		}
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.tag == Tags.playerFeet) {
+			other.transform.parent.transform.Translate(transform.right * m_velocity * Time.deltaTime * m_movile.getActualDirection(), Space.World);
+		}
+
+	}
+}
